1c712b2e8fecbcf7b6e62a537d60b415ddaa4369
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-14 13:10:09 -0700

    starting to create HTML via JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 const boardElement = document.getElementById('gameboard');
-boardElement.innerHTML = '<p>changing the DOM</p>'
+boardElement.innerHTML = `
+<!-- gameboard squares -->
+<div class="col-3 board-square ">
+    <div class="face-container">
+        <div class="facedown"></div>
+        <div class="faceup"></div>
+
+    </div>
+</div>`;
 console.log(boardElement);
